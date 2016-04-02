d5cf445975e0e93504dfd02868869bd80e699455
GilEli1 <gil1eli@hotmail.com>
2016-04-02 23:10:20 +0300

    Added files via upload

---
+++ ColorConstants.h
@@ -0,0 +1,67 @@
+// #CONSTANTS#  == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == =
+
+// Colour Constants RGB Hex
+#define CU3_COLOR_AQUA 0x00FFFF
+#define CU3_COLOR_BLACK 0x000000
+#define CU3_COLOR_BLUE 0x0000FF
+#define CU3_COLOR_CREAM 0xFFFBF0
+#define CU3_COLOR_FUCHSIA 0xFF00FF
+#define CU3_COLOR_GRAY 0x808080
+#define CU3_COLOR_GREEN 0x008000
+#define CU3_COLOR_LIME 0x00FF00
+#define CU3_COLOR_MAROON 0x8B1C62
+#define CU3_COLOR_MEDBLUE 0x0002C4
+#define CU3_COLOR_MEDGRAY 0xA0A0A4
+#define CU3_COLOR_MONEYGREEN 0xC0DCC0
+#define CU3_COLOR_NAVY 0x000080
+#define CU3_COLOR_OLIVE 0x808000
+#define CU3_COLOR_PURPLE 0x800080
+#define CU3_COLOR_RED 0xFF0000
+#define CU3_COLOR_SILVER 0xC0C0C0
+#define CU3_COLOR_SKYBLUE 0xA6CAF0
+#define CU3_COLOR_TEAL 0x008080
+#define CU3_COLOR_WHITE 0xFFFFFF
+#define CU3_COLOR_YELLOW 0xFFFF00
+
+#define CU3_CLR_NONE 0xFFFFFFFF
+#define CU3_CLR_DEFAULT 0xFF000000
+
+// Color Constants BGR Hex
+#define CU3_CLR_AQUA 0xFFFF00
+#define CU3_CLR_BLACK 0x000000
+#define CU3_CLR_BLUE 0xFF0000
+#define CU3_CLR_CREAM 0xF0FBFF
+#define CU3_CLR_FUCHSIA 0xFF00FF
+#define CU3_CLR_GRAY 0x808080
+#define CU3_CLR_GREEN 0x008000
+#define CU3_CLR_LIME 0x00FF00
+#define CU3_CLR_MAROON 0x621C8B
+#define CU3_CLR_MEDBLUE 0xC40200
+#define CU3_CLR_MEDGRAY 0xA4A0A0
+#define CU3_CLR_MONEYGREEN 0xC0DCC0
+#define CU3_CLR_NAVY 0x800000
+#define CU3_CLR_OLIVE 0x008080
+#define CU3_CLR_PURPLE 0x800080
+#define CU3_CLR_RED 0x0000FF
+#define CU3_CLR_SILVER 0xC0C0C0
+#define CU3_CLR_SKYBLUE 0xF0CAA6
+#define CU3_CLR_TEAL 0x808000
+#define CU3_CLR_WHITE 0xFFFFFF
+#define CU3_CLR_YELLOW 0x00FFFF
+
+// Color Dialog constants
+#define CU3_CC_ANYCOLOR 0x0100
+#define CU3_CC_FULLOPEN 0x0002
+#define CU3_CC_RGBINIT 0x0001
+// == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == == =
+
+
+
+
+
+
+
+
+
+
+
