c4a844100863381a6f71c36544f6755c32011057
GilEli1 <gil1eli@hotmail.com>
2016-04-02 20:34:32 +0300

    Added files via upload

---
+++ core.h
@@ -0,0 +1,53 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) <2016> <gileli121@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+
+/*
+	Long name space: "CU3_Library"
+	Short name space: "CU3"
+
+	Use whatever you want.
+
+	why short name space?
+		In case that you dont want to use the line:
+		using namespace CU3_Library;
+
+		You have to use CU3_Library::<Function / Variable>
+		In this case you can use CU3::<Function / Variable>
+		instead and it will be the same.
+
+
+	This desain allows you to easly change from here the namespace string to
+	something else if you need.
+*/
+
+
+
+#define CU3_NAMESPACE CU3_Library
+#define CU3 CU3_NAMESPACE
+
+#define CU3_NAMESPACE_START namespace CU3_NAMESPACE {
+#define CU3_NAMESPACE_END }
+
---
+++ gui.h
@@ -0,0 +1,160 @@
+#pragma once
+#include <chrono>
+
+
+#define CU3_DEFAULT_GUI_STYLE WS_MINIMIZEBOX | WS_CAPTION | WS_POPUP | WS_SYSMENU
+
+#define CU3_SW_SHOW SW_SHOW // Show window
+#define CU3_SW_HIDE SW_HIDE // Hide window
+#define CU3_SW_MINIMIZE SW_MINIMIZE // Minimize window
+#define CU3_SW_MAXIMIZE SW_MAXIMIZE // Maximize window
+#define CU3_SW_RESTORE SW_RESTORE // Undoes a window minimization
+#define CU3_SW_DISABLE -1 // Disables the window
+#define CU3_SW_ENABLE -2 // Enables the window
+//#define CU3_SW_LOCK -3 // Lock the window to avoid repainting.  ***** Not implemented
+//#define CU3_SW_UNLOCK -4 // Unlock windows to allow painting.   ***** Not implemented
+#define CU3_SW_SHOWDEFAULT SW_SHOWDEFAULT // Sets the show state based on the SW_ flag specified in the STARTUPINFO structure
+#define CU3_SW_SHOWMAXIMIZED SW_SHOWMAXIMIZED // Activates the window and displays it as a maximized window
+#define CU3_SW_SHOWMINIMIZED SW_SHOWMINIMIZED // Activates the window and displays it as a minimized window
+#define CU3_SW_SHOWMINNOACTIVE SW_SHOWMINNOACTIVE // Displays the window as a minimized window
+#define CU3_SW_SHOWNA SW_SHOWNA // Displays the window in its current state
+#define CU3_SW_SHOWNOACTIVATE SW_SHOWNOACTIVATE // Displays a window in its most recent size and position
+#define CU3_SW_SHOWNORMAL SW_SHOWNORMAL // Activates and displays a window
+
+
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+/*--------------------------------------------------------------*/
+
+CU3_NAMESPACE_START
+
+namespace CU3WINGUI {
+	WNDCLASSEX wcRootwin;
+	HWND hwndLastCreatedGUI = NULL;
+}
+
+using namespace CU3WINGUI;
+
+/*							FOR_INITIALIZATION (required only in C++)						*/	
+int InitializeRootGUI(char parm_charRootName[], HINSTANCE parm_hInstance = NULL) {
+
+
+	if (!parm_hInstance) {
+		parm_hInstance = GetModuleHandle(NULL);
+		if (!parm_hInstance) return -1; //							ERROR -1
+	}
+
+	
+	wcRootwin.cbSize = sizeof(WNDCLASSEX);
+	wcRootwin.style = 0;
+	wcRootwin.lpfnWndProc = WndProc;
+	wcRootwin.cbClsExtra = 0;
+	wcRootwin.cbWndExtra = 0;
+	wcRootwin.hInstance = parm_hInstance;
+	wcRootwin.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wcRootwin.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcRootwin.hbrBackground = (HBRUSH)(COLOR_BTNSHADOW);
+	wcRootwin.lpszMenuName = NULL;
+	wcRootwin.lpszClassName = parm_charRootName;
+	wcRootwin.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wcRootwin)) return -2; //					ERROR -2
+
+	return 1; //													NO ERROR 1
+}
+int InitializeRootGUIEx(WNDCLASSEX *parm_rootwin_wc) {
+	if (!RegisterClassEx(parm_rootwin_wc)) return -1; //				ERROR -1
+	CU3WINGUI::wcRootwin = *parm_rootwin_wc;
+}
+HINSTANCE GethInstance() {
+	return GetModuleHandle(NULL);
+}
+
+
+/*						      AUTOIT GUI FUNCTIONS							*/
+HWND GUICreate(char sTitle[], int iWidth = -1, int iHeight = -1, int iLeft = -1, int iTop = -1, DWORD dwStyle = -1,DWORD dwExStyle = NULL, HWND hwndParent = NULL) {
+
+	if (iWidth == -1) iWidth = 400; if (iHeight == -1) iHeight = 400;
+	
+	
+	if (iLeft == -1 | iTop == -1) {
+		RECT rectScreen;
+		GetWindowRect(GetDesktopWindow(), &rectScreen);
+		if (iLeft == -1) iLeft = (rectScreen.right - iWidth) >> 1;
+		if (iTop == -1) iTop = (rectScreen.bottom - iHeight) >> 1;
+	}
+
+	
+	if (dwStyle == -1) dwStyle = CU3_DEFAULT_GUI_STYLE;
+
+	HWND hGUI = CreateWindowEx(dwExStyle, wcRootwin.lpszClassName, sTitle, dwStyle, iLeft, iTop, iWidth, iHeight, hwndParent, NULL, wcRootwin.hInstance, NULL);
+	
+	if (!hGUI) return NULL; //									ERROR NULL
+
+	hwndLastCreatedGUI = hGUI;
+	return hGUI; //												NO ERROR HWND
+}
+int GUISetState(int iFlag = SW_SHOW, HWND hwndWinhandle = NULL) {
+
+	if (!hwndWinhandle) {
+		if (!hwndLastCreatedGUI) return false; //					    ERROR 0
+		hwndWinhandle = hwndLastCreatedGUI;
+	}
+	if (iFlag >= 0) {
+		ShowWindow(hwndWinhandle, iFlag);
+	}
+	else
+	{
+		switch (iFlag)
+		{
+		case CU3_SW_DISABLE:
+			EnableWindow(hwndWinhandle, false); return 1;
+
+		case CU3_SW_ENABLE:
+			EnableWindow(hwndWinhandle, true); return 1;
+
+		/* wasn't implemented:
+		case CU3_SW_LOCK:
+
+			return 1;
+
+		case CU3_SW_UNLOCK:
+			return 1;
+		*/
+
+		default:
+			return -2; //											ERROR -2
+		}
+	}
+
+	return 1; //													NO ERROR 1
+}
+
+
+
+/*							C++ DEBUG FUNCTIONS								*/
+void GUIWait(unsigned int iMsTime = 0) {
+	MSG msg;
+	if (!iMsTime) {
+		while (1) {
+			Sleep(1);
+			if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) == WM_QUIT) return;
+			 DispatchMessage(&msg);
+		}
+	}
+	else
+	{
+		clock_t iStartTime = clock();
+		do
+		{
+			Sleep(1);
+			if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) == WM_QUIT) return;
+			DispatchMessage(&msg);
+		} while (clock()-iStartTime <= iMsTime);
+	}
+}
+
+
+CU3_NAMESPACE_END
+
